0e440fbfef977d5a32af8c1f8b4f93defa060e03
timkerchmar <ptymn1447@hotmail.com>
2018-04-25 21:23:55 -0400

    initial real commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# tsthread
-Simple thread wrapper (POSIX and Win32)
+# TSThread
+Simple thread wrapper. Tested on Windows, Android, OSX, iOS.
+ 
+## Sample Usage
+
+```cpp
+TSConditionVariable printedStuff;
+
+class BufferingThread : public TSThread
+{
+public:
+    std::string name();
+    void run()
+	{
+		printf ("hello from a background thread\n");
+		printedStuff.notify();
+	}
+};
+
+void testThreads() {
+	TSMutex waitingForPrintedStuff;
+	BufferingThread bufferingThread;
+
+	waitingForPrintedStuff.lock();
+	bufferingThread.start();
+	printedStuff.wait(waitingForPrintedStuff);
+	printf("hello from parent thread\n");
+	waitingForPrintedStuff.unlock();
+}
+```
---
+++ TSThread.h
@@ -0,0 +1,55 @@
+#ifndef TSTHREAD_H
+#define TSTHREAD_H
+
+#include <sys/types.h>
+#include "TSType.h"
+
+#ifdef WIN32
+#include <Windows.h>
+#else
+#include <pthread.h>
+#endif
+
+class TSMutex
+{
+public:
+    TSMutex();
+#ifdef WIN32
+    CRITICAL_SECTION handle;
+#else
+    pthread_mutex_t handle;
+#endif
+    void lock();
+    void unlock();
+};
+
+class TSConditionVariable
+{
+public:
+    TSConditionVariable();
+    
+#ifdef WIN32
+    CONDITION_VARIABLE handle;
+#else
+    pthread_cond_t handle;
+#endif
+    void wait(TSMutex &mutex);
+    void notify();
+};
+
+class TSThread
+{
+public:
+    void start();
+    
+    virtual void run() = 0;
+    virtual std::string name() = 0;
+
+#ifdef WIN32
+	HANDLE handle;
+#else
+	pthread_t handle;
+#endif
+};
+
+#endif //TSTHREAD_H
